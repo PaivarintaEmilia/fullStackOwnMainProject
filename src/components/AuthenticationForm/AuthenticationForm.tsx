0258c9655f5bd863226358780ae50eee9c560a27
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2024-10-20 12:29:28 +0200

    Aloitetaan /login ja /register toimintojen luominen.

--- src/components/AuthenticationForm/AuthenticationForm.tsx
+++ src/components/AuthenticationForm/AuthenticationForm.tsx
@@ -2,78 +2,51 @@ import React, { useState } from 'react';
 import ButtonComponent from '../../common/Button';
 import InputField from '../../common/InputField';
 import style from "./AuthenticationForm.module.css";
-import { useNavigate } from 'react-router-dom';
 
 interface AuthenticateFormProps {
     title: string,
+    onSubmit: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    onChangeUsername: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    onChangePassword: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    valueUsername: string,
+    valuePassword: string,
+    text: string
 }
 
 const AuthenticationForm: React.FC<AuthenticateFormProps> = ({
     title,
+    onSubmit,
+    onChangeUsername,
+    onChangePassword,
+    valueUsername,
+    valuePassword,
+    text,
 }) => {
 
-    // Tilamuuttujat, usernamen ja passwordin käyttöön
-    const [username, setUsername] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
 
-    // Navigointi
-    const navigate = useNavigate();
-
-    // submitform-funktio, kun lähetetään username ja password backendille
-    const submitForm = async (event: React.FormEvent) => {
-        event.preventDefault();
-    
-        const signInData = {
-            username,
-            password
-        };
-
-        console.log("SignInData: ", signInData);
-    
-        try {
-            const response = await fetch('http://127.0.0.1:8000/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json', // Oikea kirjoitusasu
-                    'Cache-Control': 'no-cache',
-                    'Accept': '*/*',
-                },
-                body: JSON.stringify(signInData),
-            });
-    
-            if (!response.ok) {
-                throw new Error('Back-end -yhteydessä on ongelma.');
-            }
-    
-            navigate('/home'); // Navigoi, jos rekisteröinti onnistuu
-    
-        } catch (error) {
-            console.error("Virhe /register-routen post-requestissa:", error);
-        }
-    };
     
 
     return (
         <div className={style.authenticationFormContainer}>
-            <form onSubmit={submitForm}>
+            <form onSubmit={onSubmit}>
                 <h2>{title}</h2>
                 <InputField
                     name='username'
                     type='text'
                     placeholder='Enter username'
-                    value={username}
+                    value={valueUsername}
                     className=''
                     id='global-input'
-                    onChange={(e) => setUsername(e.target.value)}
+                    onChange={onChangeUsername}
                 />
                 <InputField
                     name='password'
                     type='password'
                     placeholder='Create password'
-                    value={password}
+                    value={valuePassword}
                     className=''
                     id='global-input' // TODO: ei voi olla kahta komponenttia samalla id:llä.
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={onChangePassword}
                 />
 
                 <ButtonComponent
@@ -82,7 +55,7 @@ const AuthenticationForm: React.FC<AuthenticateFormProps> = ({
                     value=''
                     className=''
                     id='global-btn'
-                    text='Login'
+                    text={text}
                     onClick={() => console.log("Todo määritä tämä myöhemmin tai muokkaa niin ettei tarvita.")} />
                 <ButtonComponent
                     name=''
---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import style from "./Authentication.module.css"
+import Hero from '../../components/Hero/Hero';
+import AuthenticationForm from '../../components/AuthenticationForm/AuthenticationForm';
+import { useNavigate } from 'react-router-dom';
+
+const Register: React.FC = () => {
+
+        // Tilamuuttujat, usernamen ja passwordin käyttöön
+        const [username, setUsername] = useState<string>('');
+        const [password, setPassword] = useState<string>('');
+    
+        // Navigointi
+        const navigate = useNavigate();
+    
+        // submitform-funktio, kun lähetetään username ja password backendille
+        const submitForm = async (event: React.FormEvent) => {
+            event.preventDefault();
+        
+            const signInData = {
+                username,
+                password
+            };
+    
+            console.log("SignInData: ", signInData);
+        
+            try {
+                const response = await fetch('http://127.0.0.1:8000/register', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json', // Oikea kirjoitusasu
+                        'Cache-Control': 'no-cache',
+                        'Accept': '*/*',
+                    },
+                    body: JSON.stringify(signInData),
+                });
+        
+                if (!response.ok) {
+                    throw new Error('Back-end -yhteydessä on ongelma.');
+                }
+        
+                navigate('/home'); // Navigoi, jos rekisteröinti onnistuu
+        
+            } catch (error) {
+                console.error("Virhe /register-routen post-requestissa:", error);
+            }
+        };
+
+  return (
+    <div className={style.mainContainer}>
+      <div className={style.heroContainer}>
+        <Hero heroTitle={'Expense Tracker'} titleTag={'SignIn or create a new user from Register-route.'} />
+      </div>
+      <div className={style.authenticationContainer}>
+        <AuthenticationForm 
+          title={'Register'} 
+          onSubmit={submitForm}
+          onChangeUsername={(e) => setUsername(e.target.value)}
+          onChangePassword={(e) => setPassword(e.target.value)}
+          text='Register'
+          valuePassword={password}
+          valueUsername={username}
+         />
+      </div>
+
+      
+    </div>
+  );
+}
+
+export default Register;
